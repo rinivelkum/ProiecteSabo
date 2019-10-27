ce4f93c25d2a6b25ff963c91517676c982f33d45
unknown <alk9mm@gmail.com>
2019-10-27 19:40:03 +0200

    1.1 - Add print_playground() method for printing the matrix.

--- CandyCrush/base.py
+++ CandyCrush/base.py
@@ -1,8 +1,11 @@
 import random
 
 def start_playground():
-    playground = [[random.choice((1,2,3,4)) for x in range(11)] for y in range(11)]
-    for rand in playground:
+    return [[random.choice((1,2,3,4)) for x in range(11)] for y in range(11)]
+def print_playground():
+    temp = zip(*reversed(playground))
+    for rand in temp:
         print(rand)
 
-start_playground()
+playground = start_playground()
+print_playground()
